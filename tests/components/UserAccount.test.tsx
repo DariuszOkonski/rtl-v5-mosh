b68dfde547a24eb717042bc1521da1f725f27599
Dariusz Okoński <darek200180@gmail.com>
2024-10-15 20:38:02 +0200

    add more test

--- tests/components/UserAccount.test.tsx
+++ tests/components/UserAccount.test.tsx
@@ -31,3 +31,30 @@ describe('UserAccount MyVersion', () => {
     expect(buttonEdit).not.toBeInTheDocument();
   });
 });
+
+describe('UserAccount MoshVersion', () => {
+  it('should render user name', () => {
+    const user: User = { id: 1, name: 'Mosh' };
+    render(<UserAccount user={user} />);
+
+    expect(screen.getByText(user.name)).toBeInTheDocument();
+  });
+
+  it('should render edit button if user is admin', () => {
+    const user: User = { id: 1, name: 'Mosh', isAdmin: true };
+    render(<UserAccount user={user} />);
+
+    const button = screen.getByRole('button');
+
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/edit/i);
+  });
+
+  it('should not render edit button if user is not admin', () => {
+    const user: User = { id: 1, name: 'Mosh' };
+    render(<UserAccount user={user} />);
+
+    const button = screen.queryByRole('button');
+    expect(button).not.toBeInTheDocument();
+  });
+});
