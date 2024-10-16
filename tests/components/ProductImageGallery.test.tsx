78f7c334ed14d7832a1c25e909c212b03a044023
Dariusz Okoński <darek200180@gmail.com>
2024-10-16 20:10:21 +0200

    add tests for termsandconditions

--- tests/components/ProductImageGallery.test.tsx
+++ tests/components/ProductImageGallery.test.tsx
@@ -29,9 +29,9 @@ describe('ProductImageGallery MoshVersion', () => {
 
     const images = screen.getAllByRole('img');
     expect(images).toHaveLength(2);
+
     // expect(images[0]).toHaveAttribute('src', imageUrls[0]);
     // expect(images[1]).toHaveAttribute('src', imageUrls[1]);
-
     imageUrls.forEach((url, index) => {
       expect(images[index]).toHaveAttribute('src', url);
     });
