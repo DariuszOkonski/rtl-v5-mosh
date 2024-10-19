a4468284fc086522f6c6b40c9c4d4b03e2f3c96a
Dariusz Okoński <darek200180@gmail.com>
2024-10-19 12:27:58 +0200

    add test for searchbox

--- tests/components/SearchBox.test.tsx
+++ tests/components/SearchBox.test.tsx
@@ -4,15 +4,35 @@ import userEvent from '@testing-library/user-event';
 
 describe('SearchBox', () => {
   const renderSearchBox = () => {
-    render(<SearchBox onChange={vi.fn()} />);
+    const onChange = vi.fn();
+    render(<SearchBox onChange={onChange} />);
 
     return {
       input: screen.getByPlaceholderText(/search/i),
+      user: userEvent.setup(),
+      onChange,
     };
   };
 
   it('should render an input field for searching', () => {
     const { input } = renderSearchBox();
     expect(input).toBeInTheDocument();
+  });
+
+  it('should call onChange when Enter is pressed', async () => {
+    const { input, onChange, user } = renderSearchBox();
+
+    const searchTerm = 'SearchTerm';
+    await user.type(input, searchTerm + '{enter}');
+
+    expect(onChange).toHaveBeenCalledWith(searchTerm);
+  });
+
+  it('should not call onChange if input field is empty', async () => {
+    const { input, onChange, user } = renderSearchBox();
+
+    await user.type(input, '{enter}');
+
+    expect(onChange).not.toHaveBeenCalled();
   });
 });
