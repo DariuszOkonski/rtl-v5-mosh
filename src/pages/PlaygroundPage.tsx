b855065bef0487d78463dfd13c3fdda9c60a087e
Dariusz Okoński <darek200180@gmail.com>
2024-10-19 12:53:51 +0200

    add test for ToastDemo

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,11 +1,19 @@
 // import ExpandableText from '../components/ExpandableText';
 // import SearchBox from '../components/SearchBox';
-import TagList from '../components/TagList';
+// import TagList from '../components/TagList';
 // import Onboarding from '../components/Onboarding';
 // import TermsAndConditions from '../components/TermsAndConditions';
 
+import { Toaster } from 'react-hot-toast';
+import ToastDemo from '../components/ToastDemo';
+
 const PlaygroundPage = () => {
-  return <TagList />;
+  return (
+    <>
+      <ToastDemo />
+      <Toaster />
+    </>
+  );
 };
 
 export default PlaygroundPage;
