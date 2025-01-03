062bae8f39ec7a728a9a4ede3ab3febaddb1977e
Dariusz Okoński <darek200180@gmail.com>
2025-01-03 13:14:21 +0100

    add tests for product detail

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -1,4 +1,5 @@
 import { http, HttpResponse } from 'msw';
+import { products } from './data';
 
 export const handlers = [
   http.get('/categories', () => {
@@ -10,37 +11,18 @@ export const handlers = [
   }),
 
   http.get('/products', () => {
-    return HttpResponse.json([
-      { id: 1, name: 'Product 1' },
-      { id: 2, name: 'Product 2' },
-      { id: 3, name: 'Product 3' },
-    ]);
+    return HttpResponse.json(products);
+  }),
+
+  http.get('/products/:id', ({ params }) => {
+    const id = parseInt(params.id as string);
+
+    const product = products.find((p) => p.id === id);
+
+    if (!product) {
+      return HttpResponse.json(null, { status: 404 });
+    }
+
+    return HttpResponse.json(product);
   }),
 ];
-
-// import { http, HttpResponse } from 'msw';
-// import { products } from './data';
-
-// export const handlers = [
-//   http.get('/categories', () => {
-//     return HttpResponse.json([
-//       { id: 1, name: 'Electronics' },
-//       { id: 2, name: 'Beauty' },
-//       { id: 3, name: 'Gardening' },
-//     ]);
-//   }),
-//   http.get('/products', () => {
-//     return HttpResponse.json(products);
-//   }),
-//   http.get('/products/:id', ({ params }) => {
-//     const id = parseInt(params.id.toString());
-
-//     const product = products.find((p) => p.id === id);
-
-//     if (!product) {
-//       return HttpResponse.json(null, { status: 404 });
-//     }
-
-//     return HttpResponse.json(product);
-//   }),
-// ];
